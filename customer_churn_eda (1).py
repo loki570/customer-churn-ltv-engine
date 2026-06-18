a4e8f8cf61a1e7a2c7a6ed4635f39fec60a3bd8c
Gundluru lokanath reddy <lokanathreddygundluru@gmail.com>
2026-06-18 21:16:55 +0530

    eda
    code file

---
+++ customer_churn_eda (1).py
@@ -0,0 +1,46 @@
+# Load Dataset
+import pandas as pd
+df=pd.read_excel(r"C:\Users\abc\Documents\Customer Churn Prediction & Lifetime Value (1) cleaned dataset.xlsx")
+print(df)
+
+
+import pandas as pd
+df=pd.read_excel(r"C:\Users\abc\Documents\Customer Churn Prediction & Lifetime Value (1) cleaned dataset.xlsx")
+print(df.head())
+print(df.shape)
+
+#Dataset Information
+import pandas as pd
+df=pd.read_excel(r"C:\Users\abc\Documents\Customer Churn Prediction & Lifetime Value (1) cleaned dataset.xlsx")
+print(df.info())
+
+# Summary Statistics
+import pandas as pd
+df=pd.read_excel(r"C:\Users\abc\Documents\Customer Churn Prediction & Lifetime Value (1) cleaned dataset.xlsx")
+print(df.describe())
+
+#Check missing values
+import pandas as pd
+df=pd.read_excel(r"C:\Users\abc\Documents\Customer Churn Prediction & Lifetime Value (1) cleaned dataset.xlsx")
+print(df.isnull().sum())
+
+#Check Duplicate Records
+import pandas as pd
+df=pd.read_excel(r"C:\Users\abc\Documents\Customer Churn Prediction & Lifetime Value (1) cleaned dataset.xlsx")
+print(df.duplicated().sum())
+
+#Analyze Churn Distribution
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+df=pd.read_excel(r"C:\Users\abc\Documents\Customer Churn Prediction & Lifetime Value (1) cleaned dataset.xlsx")
+churn_count = df['Churn'].value_counts()
+churn_percentage = df['Churn'].value_counts(normalize=True) * 100
+print("Customer Count:")
+print(churn_count)
+print("\nCustomer Percentage:")
+print(churn_percentage.round(2))
+sns.countplot(x='Churn', data=df)
+plt.title("Churn Distribution")
+plt.show()
+
